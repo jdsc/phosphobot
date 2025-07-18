b2f745839ac08c8a7992475a91f90f191d3dbbfa
Hiroyuki-Kaneko-0505 <hiroyuki.kaneko@jdsc.ai>
2025-07-18 15:08:31 +0900

    Corrected inference code (deleted needless RGB conversion, and treated time count)

--- scripts/quickstart_ai.py
+++ scripts/quickstart_ai.py
@@ -34,10 +34,7 @@ joint_ids = [1, 2, 3, 4, 5, 6]
 actions_queue: deque = deque([]) # phosphobot/am/act.pyを参考にしたqueue
 
 while True:
-    start_time = time.perf_counter()
-
     camera0_frame = allcameras.get_rgb_frame(camera_id=0, resize=(320, 240)) #公式は(240, 320)だが、縦長になる。cv2はH,Wなのでこれで横長。 #id0:wrist camera
-    camera0_frame = cv2.cvtColor(camera0_frame, cv2.COLOR_RGB2BGR) # RGBに変換
     camera1_frame = allcameras.get_rgb_frame(camera_id=1, resize=(320, 240)) #1: 俯瞰カメラ
     # camera2_frame = allcameras.get_rgb_frame(camera_id=2, resize=(320, 240)) #2: PCカメラ
 
@@ -62,33 +59,29 @@ while True:
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     
-    if len(actions_queue) == 0:
-        actions = model(inputs) #6要素のlist →修正後：6要素のlist×時系列分がネスト
-        actions_queue.extend(actions)
-    actions = actions_queue.popleft()
-
-    # actions = actions[0].astype(np.float64).tolist()
+    actions = model(inputs) #6要素のlist →修正後：6要素のlist×時系列分がネスト
 
     # Send the new joint postion to the robot
-    inputs = {
-        "angles": actions[0].astype(np.float64).tolist(),
-        "unit": "rad",
-        "joints_ids": joint_ids,
-    }
+    for action in actions:
+        start_time = time.perf_counter()
+        inputs = {
+            "angles": action[0].tolist(),
+            "unit": "rad",
+            "joints_ids": joint_ids,
+        }
 
-    httpx.post(
-        url = f"{PHOSPHOBOT_API_URL}/joints/write", 
-        json=inputs,
-        headers={"accept": "application/json"},
-        params={"robot_id": 0}, # 1
-    )
+        httpx.post(
+            url = f"{PHOSPHOBOT_API_URL}/joints/write", 
+            json=inputs,
+            headers={"accept": "application/json"},
+            params={"robot_id": 0}, # 1
+        )
 
-    # Wait to respect frequency control (30 Hz)
-    # time.sleep(1 / 30)
-    elapsed_time = time.perf_counter() - start_time
-    # 目標周期から経過時間を引いて、待機すべき時間を計算
-    sleep_time = 1/FPS - elapsed_time
-    
-    # 待機時間が0より大きい場合のみ待機する
-    if sleep_time > 0:
-        time.sleep(sleep_time)
+        # Wait to respect frequency control (30 Hz)
+        # time.sleep(1 / 30)
+        elapsed_time = time.perf_counter() - start_time
+        # 目標周期から経過時間を引いて、待機すべき時間を計算
+        sleep_time = 1/FPS - elapsed_time
+        # 待機時間が0より大きい場合のみ待機する
+        if sleep_time > 0:
+            time.sleep(sleep_time)
